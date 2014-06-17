e8589629c919ceac9cb0caad94adc1d2e9621a92
Jordan Orelli <jorelli@etsy.com>
2014-06-17 20:35:33 +0000

    fmt

--- registrar_other.go
+++ registrar_other.go
@@ -1,23 +1,24 @@
 // +build !windows
 
 package main
+
 import (
-  "encoding/json"
-  "os"
-  "log"
+	"encoding/json"
+	"log"
+	"os"
 )
 
 func WriteRegistry(state map[string]*FileState, path string) {
-  // Open tmp file, write, flush, rename
-  file, err := os.Create(".lumberjack.new")
-  if err != nil {
-    log.Printf("Failed to open .lumberjack.new for writing: %s\n", err)
-    return
-  }
-  defer file.Close()
+	// Open tmp file, write, flush, rename
+	file, err := os.Create(".lumberjack.new")
+	if err != nil {
+		log.Printf("Failed to open .lumberjack.new for writing: %s\n", err)
+		return
+	}
+	defer file.Close()
 
-  encoder := json.NewEncoder(file)
-  encoder.Encode(state)
+	encoder := json.NewEncoder(file)
+	encoder.Encode(state)
 
-  os.Rename(".lumberjack.new", path)
+	os.Rename(".lumberjack.new", path)
 }
