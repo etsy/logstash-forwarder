0bb9b128f4a47d03b205b713afceb84cfb5ccb80
Jordan Orelli <jorelli@etsy.com>
2014-07-22 23:22:41 +0000

    publishers now send their own payloads
    there are some horrible goto's in here that will be removed.

--- publisher1.go
+++ publisher1.go
@@ -52,68 +52,55 @@ func (p *Publisher) publish(input chan eventPage, registrar chan eventPage, conf
 			continue
 		}
 		p.sequence += uint32(len(page))
-
 		compressed_payload := p.buffer.Bytes()
 
-		// Send buffer until we're successful...
-		oops := func(err error) {
-			// TODO(sissel): Track how frequently we timeout and reconnect. If we're
-			// timing out too frequently, there's really no point in timing out since
-			// basically everything is slow or down. We'll want to ratchet up the
-			// timeout value slowly until things improve, then ratchet it down once
-			// things seem healthy.
+	SendPayload:
+		if err := p.sendPayload(len(page), compressed_payload, config.timeout); err != nil {
 			log.Printf("Socket error, will reconnect: %s\n", err)
 			time.Sleep(1 * time.Second)
 			p.socket.Close()
 			p.connect(config, p.id)
+			goto SendPayload
 		}
 
-	SendPayload:
-		for {
-			// Abort if our whole request takes longer than the configured
-			// network timeout.
-			p.socket.SetDeadline(time.Now().Add(config.timeout))
-
-			w := &errorWriter{Writer: p.socket}
-
-			// Set the window size to the length of this payload in events.
-			w.Write([]byte("1W"))
-			binary.Write(w, binary.BigEndian, uint32(len(page)))
-
-			// Write compressed frame
-			w.Write([]byte("1C"))
-			binary.Write(w, binary.BigEndian, uint32(len(compressed_payload)))
-			w.Write(compressed_payload)
-
-			if err := w.Err(); err != nil {
-				oops(err)
-				continue
+		// read ack
+		response := make([]byte, 0, 6)
+		ackbytes := 0
+		for ackbytes != 6 {
+			n, err := p.socket.Read(response[len(response):cap(response)])
+			if err != nil {
+				log.Printf("Read error looking for ack: %s\n", err)
+				p.socket.Close()
+				p.connect(config, p.id)
+				goto SendPayload // retry sending on new connection
+			} else {
+				ackbytes += n
 			}
-
-			// read ack
-			response := make([]byte, 0, 6)
-			ackbytes := 0
-			for ackbytes != 6 {
-				n, err := p.socket.Read(response[len(response):cap(response)])
-				if err != nil {
-					log.Printf("Read error looking for ack: %s\n", err)
-					p.socket.Close()
-					p.connect(config, p.id)
-					continue SendPayload // retry sending on new connection
-				} else {
-					ackbytes += n
-				}
-			}
-
-			// TODO(sissel): verify ack
-			// Success, stop trying to send the payload.
-			break
 		}
+
+		// TODO(sissel): verify ack
 
 		// Tell the registrar that we've successfully sent these events
 		registrar <- page
 	} /* for each event payload */
 
+}
+
+func (p *Publisher) sendPayload(size int, payload []byte, timeout time.Duration) error {
+	p.socket.SetDeadline(time.Now().Add(timeout))
+
+	w := &errorWriter{Writer: p.socket}
+
+	// Set the window size to the length of this payload in events.
+	w.Write([]byte("1W"))
+	binary.Write(w, binary.BigEndian, uint32(size))
+
+	// Write compressed frame
+	w.Write([]byte("1C"))
+	binary.Write(w, binary.BigEndian, uint32(len(payload)))
+	w.Write(payload)
+
+	return w.Err()
 }
 
 func (p *Publisher) connect(config *NetworkConfig, id int) {
